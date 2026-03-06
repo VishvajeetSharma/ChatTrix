b832728c77a4f90c173b324abdef98658eba4f2b
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-06 18:33:12 +0530

    created some auth route

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,8 +1,10 @@
 import "dotenv/config"
 import express from "express";
-
+import router from "./routes/authRoute.js";
 const  app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(`/api/auth`, router);
 
 app.listen(PORT, ()=>{
   console.log(`Sever is Running on:  http://localhost:${PORT}`);
